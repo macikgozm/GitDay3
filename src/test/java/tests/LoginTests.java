8608937b48c10cd36dcc0e3011800bba630a58eb
Mehmet Acikgoz <macikgozm@gmail.com>
2019-02-01 20:31:01 -0600

    Commit 4: clean code

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
 
+
     WebDriver driver;
 
     @BeforeMethod
@@ -24,6 +25,8 @@ public class LoginTests {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+
+
     @Test
     public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
@@ -32,16 +35,21 @@ public class LoginTests {
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
 
-    @AfterMethod
-    public void logOutTest(){
+    @Test
+    public void logOutTest() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
 
-        driver.findElement(By.id("ct100_logout")).click();
+        driver.findElement(By.id("ctl00_logout")).click();
 
         Assert.assertEquals(driver.getTitle(), "Web Orders Login");
 
     }
 
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.close();
+    }
 }
