7453767987594a685cf6678f4770da7bbb6a39a0
Mehmet Acikgoz <macikgozm@gmail.com>
2019-02-09 19:14:33 -0600

    commit beofere the start the new course

---
+++ src/test/java/LoginStep.java
@@ -0,0 +1,13 @@
+import cucumber.api.PendingException;
+
+/**
+ * Created by macik on 2/2/2019.
+ */
+public class LoginStep {
+    public LoginStep() {
+        And("^I enter the username as admin and password as admin$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+    }
+}
